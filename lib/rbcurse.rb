53571ea8a7af146d6b4983af21de8140d5a51f2e
Rahul Kumar <rahulbenegal@gmail.com>
2011-11-20 23:53:36 +0530

    initial commit

---
+++ lib/rbcurse.rb
@@ -0,0 +1,8 @@
+require 'ffi-ncurses' 
+require 'rbcurse/core/system/ncurses'
+require 'rbcurse/core/system/window'
+require 'rbcurse/core/widgets/rwidget'
+require 'rbcurse/core/util/rdialogs'
+class Rbcurse
+  VERSION = '0.0.0'
+end
