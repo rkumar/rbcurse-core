eea3eca759110dd2776be26eafd285a0ba68df8a
Rahul Kumar <rahulbenegal@gmail.com>
2011-11-26 18:20:19 +0530

    minor tweaks

--- examples/tasks.rb
+++ examples/tasks.rb
@@ -2,7 +2,7 @@ require 'rbcurse/core/util/app'
 require 'rbcurse/core/widgets/rlist'
 
 App.new do 
-  @default_prefix = "_"
+  @default_prefix = " "
   header = app_header "rbcurse #{Rbcurse::VERSION}", :text_center => "Task List", :text_right =>"New Improved!"
 
   message "Press F10 or qq to escape from here"
@@ -10,7 +10,7 @@ App.new do
   file = "data/todo.txt"
   alist = File.open(file,'r').readlines if File.exists? file
   #flow :margin_top => 1, :item_width => 50 , :height => FFI::NCurses.LINES-2 do
-  stack :margin_top => 1, :width => :expand, :height => FFI::NCurses.LINES-2 do
+  stack :margin_top => 1, :width => :expand, :height => FFI::NCurses.LINES-4 do
 
     #task = field :label => "    Task:", :display_length => 50, :maxlen => 80, :bgcolor => :cyan, :color => :black
     #pri = field :label => "Priority:", :display_length => 1, :maxlen => 1, :type => :integer, 
@@ -18,7 +18,7 @@ App.new do
     #pri.overwrite_mode = true
     # u,se voerwrite mode for this TODO and catch exception
 
-    lb = listbox :list => alist.sort, :title => "[ todos ]", :height_pc => 80, :name => "tasklist"
+    lb = listbox :list => alist.sort, :title => "[ todos ]", :height_pc => 100, :name => "tasklist"
     lb.bind_key(?d){ 
       if confirm("Delete #{lb.current_value} ?")
         lb.delete_at lb.current_index 
@@ -26,25 +26,26 @@ App.new do
       end
     }
     lb.bind_key(?e){ 
-      if ((value = get_string("Edit Task:", :width => 80, :default => lb.current_value)) != nil)
+      if ((value = get_string("Edit Task:", :width => 80, :default => lb.current_value, :maxlen => 80, :display_length => 70)) != nil)
         lb[lb.current_index]=value
       end
     }
     lb.bind_key(?A){ 
 
       # ADD
-    task = Field.new :label => "    Task:", :display_length => 50, :maxlen => 80, :bgcolor => :cyan, :color => :black,
+    task = Field.new :label => "    Task:", :display_length => 70, :maxlen => 80, :bgcolor => :cyan, :color => :black,
     :name => 'task'
     pri = Field.new :label => "Priority:", :display_length => 1, :maxlen => 1, :type => :integer, 
       :valid_range => 1..9, :bgcolor => :cyan, :color => :black , :default => "5", :name => 'pri'
     pri.overwrite_mode = true
     config = {}
-    config[:width] = 70
+    config[:width] = 80
     config[:title] =  "New Task"
     tp = MessageBox.new config do
       item task
       item pri
       button_type :ok_cancel
+      default_button 0
     end
     index = tp.run
     if index == 0 # OK
@@ -58,6 +59,7 @@ App.new do
       #return nil
     end
     }
+    # decrease priority
     lb.bind_key(?D){ 
       line = lb.current_value
       p = line[1,1].to_i
@@ -68,6 +70,7 @@ App.new do
         lb.list(lb.list.sort)
       end
     }
+    # increase priority
     lb.bind_key(?I){ 
       line = lb.current_value
       p = line[1,1].to_i
@@ -82,19 +85,21 @@ App.new do
         # Maybe setform_row needs to be called
       end
     }
+    # mark as done
     lb.bind_key(?x){ 
       line = lb.current_value
       line[0,1] = "x"
       lb[lb.current_index]=line
       lb.list(lb.list.sort)
     }
+    # flag task with a single character
     lb.bind_key(?!){ 
       line = lb.current_value.chomp
       value = get_string("Flag for #{line}. Enter one character.", :maxlen => 1, :display_length => 1)
       #if ((value = get_string("Edit Task:", :width => 80, :default => lb.current_value)) != nil)
         #lb[lb.current_index]=value
       #end
-      if value && value[0,1] != " "
+      if value ##&& value[0,1] != " "
         line[0,1] = value[0,1]
         lb[lb.current_index]=line
         lb.list(lb.list.sort)
@@ -118,11 +123,11 @@ App.new do
       :footer_mnemonic_color_pair => gw }
 
   @window.confirm_close_command do
-    confirm "Sure you wanna quit?"
+    confirm "Sure you wanna quit?", :default_button => 1
   end
   @window.close_command do
     w = @form.by_name["tasklist"]
-    if confirm("Save tasks?")
+    if confirm("Save tasks?", :default_button => 0)
       system("cp #{file} #{file}.bak")
       File.open(file, 'w') {|f| 
         w.list.each { |e|  
