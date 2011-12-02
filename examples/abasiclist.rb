6b34b531d6ccf2d2261995ff42dfb3fe6227fb4e
Rahul Kumar <rahulbenegal@gmail.com>
2011-12-03 00:22:04 +0530

    abasiclist.rb fixed margins and used dark grey 256 for border

--- examples/abasiclist.rb
+++ examples/abasiclist.rb
@@ -3,21 +3,20 @@ require 'rbcurse/core/widgets/rlist'
 
 # just a simple test to ensure that rbasiclistbox is running inside a container.
 App.new do 
-  header = app_header "rbcurse #{Rbcurse::VERSION}", :text_center => "Basic List Demo", :text_right =>"New Improved!", :color => :black, :bgcolor => :white, :attr => :bold 
+  header = app_header "rbcurse #{Rbcurse::VERSION}", :text_center => "Basic List Demo", :text_right =>"New Improved!", :color => :white, :bgcolor => 234 #, :attr => :bold 
   message "Press F10 to escape from here"
 
-  #list = %W{ bhikshu boddisattva avalokiteswara mu mun kwan paramita prajna samadhi sutra shakyamuni }
   alist = File.open("data/brew.txt",'r').readlines
   list2 = File.open("data/gemlist.txt",'r').readlines
   lb = nil
   #vimsplit :row => 1, :col => 0, :suppress_borders => false, :width => 60, :height => Ncurses.LINES-2, :weight => 0.4, :orientation => :VERTICAL do |s|
-  flow :margin_top => 1, :item_width => 50 , :height => FFI::NCurses.LINES-2 do
+  flow :margin_top => 1, :height => FFI::NCurses.LINES-2 do
     lb = listbox :list => alist, :suppress_borders => false, :title => "[ brew packages ]",
-      :left_margin => 1
+      :left_margin => 1, :width_pc => 50
     lb.show_selector = false
     
     lb2 = listbox :list => list2, :justify => :left, :title => "[ gems ]", :suppress_borders => false,
-      :left_margin => 1
+      :left_margin => 1, :width_pc => 50
     end
     #label({:text => "checking overwrite from list", :row => 10, :col => 60})
     #label({:text => "checking overwrite from list 1", :row => 11, :col => 60})
