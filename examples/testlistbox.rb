22401519dfcb7ec52a631a4dc645452d35b77a8b
Rahul Kumar <rahulbenegal@gmail.com>
2011-12-03 00:22:28 +0530

    testlistbox.rb using ri output so it can be useful demo

--- examples/testlistbox.rb
+++ examples/testlistbox.rb
@@ -2,6 +2,7 @@
 require 'logger'
 require 'rbcurse'
 require 'rbcurse/core/widgets/rlist'
+require 'rbcurse/core/widgets/rtextview'
 require 'rbcurse/core/include/vieditable'
 #require 'rbcurse/experimental/widgets/undomanager'
 class RubyCurses::List
@@ -33,51 +34,66 @@ if $0 == __FILE__
       r = 1; fc = 1;
 
       $results = Variable.new
-      $results.value = "A list with vim-like key bindings. Try j k gg G o O C dd f<char> w yy p P. "
-      var = RubyCurses::Label.new @form, {'text_variable' => $results, "row" => FFI::NCurses.LINES-2, "col" => fc, "display_length" => 100, "height" => 1}
+      $results.value = "F10 quits. Try j k gg G o O C dd f<char> w yy p P / . Press ENTER on Class or Method"
+      var = RubyCurses::Label.new @form, {'text_variable' => $results, "row" => FFI::NCurses.LINES-2, 
+        "col" => fc, "display_length" => 100, "height" => 1}
       h = FFI::NCurses.LINES-3
       file = "./data/ports.txt"
-      mylist = File.open(file,'r').readlines 
+      #mylist = File.open(file,'r').readlines 
+      mylist = `ri -f bs`.split("\n")
+      w = 25
       #0.upto(100) { |v| mylist << "#{v} scrollable data" }
       listb = List.new @form do
         name   "mylist" 
         row  r 
         col  1 
-        width 80
+        width w
         height h
-        #         list mylist
         list mylist
-        #selection_mode :SINGLE
+        selection_mode :SINGLE
         show_selector true
-        row_selected_symbol "[X] "
-        row_unselected_symbol "[ ] "
-        title " A long list "
+        #row_selected_symbol "[X] "
+        #row_unselected_symbol "[ ] "
+        title " Ruby Classes "
         #title_attrib 'reverse'
       end
       listb.one_key_selection = false # this allows us to map keys to methods
       listb.vieditable_init_listbox
       include Io
       listb.bind_key(?r){ get_file("Get a file:", 70) }
+      listb.bind(:PRESS) { 
+        w = @form.by_name["tv"]; 
+        lines = `ri -f bs #{listb.text}`.split("\n")
+        #w.set_content(lines); 
+        w.formatted_text(lines, :ansi)
+        w.title = listb.text
+      }
+
+      tv = RubyCurses::TextView.new @form, :row => r, :col => w+1, :height => h, :width => FFI::NCurses.COLS-w-1,
+      :name => "tv", :title => "Press Enter on method"
+      tv.set_content ["Press Enter on list to view ri information in this area.", 
+        "Press ENTER on method name to see details"]
+      tv.bind(:PRESS) { |ev|
+        w = ev.word_under_cursor.strip
+        text = `ri -f bs #{tv.title}.#{w}` rescue "Not details for #{w}"
+        text = text.split("\n")
+        temptext = ["sending in","some text"]
+        view(temptext) do |t|
+          t.formatted_text(text, :ansi)
+        end
+      }
+
       #undom = SimpleUndo.new listb
 
-      #listb.list.insert 55, "hello ruby", "so long python", "farewell java", "RIP .Net"
 
-    # just for demo, lets scroll the text view as we scroll this.
-    #        listb.bind(:ENTER_ROW, @textview) { |alist, tview| tview.top_row alist.current_index }
-
-    #list = ListDataModel.new( %w[spotty tiger panther jaguar leopard ocelot lion])
-    #list.bind(:LIST_DATA_EVENT) { |lde| $message.value = lde.to_s; $log.debug " STA: #{$message.value} #{lde}"  }
-    #list.bind(:ENTER_ROW) { |obj| $message.value = "ENTER_ROW :#{obj.current_index} : #{obj.selected_item}    "; $log.debug " ENTER_ROW: #{$message.value} , #{obj}"  }
-
+=begin
     # using ampersand to set mnemonic
     col = FFI::NCurses.COLS-10
     row = FFI::NCurses.LINES-2
     cancel_button = Button.new @form do
-      #variable $results
-      text "&Cancel"
+      text "&Close"
       row row
       col col
-      default_button true
     end
     cancel_button.command { |form| 
       if confirm("Do your really want to quit?")
@@ -87,13 +103,13 @@ if $0 == __FILE__
       end
     }
 
+=end
 
     @form.repaint
     @window.wrefresh
     Ncurses::Panel.update_panels
-    while((ch = @window.getchar()) != KEY_F1 )
+    while((ch = @window.getchar()) != KEY_F10 )
       @form.handle_key(ch)
-      #@form.repaint
       @window.wrefresh
     end
   end
