58d3585d0c422fbaaf817680b9ccacae5c786512
Rahul Kumar <rahulbenegal@gmail.com>
2011-12-11 22:40:32 +0530

    listselectable.rb check if some other key globally assigned for row_selector

--- lib/rbcurse/core/include/listselectable.rb
+++ lib/rbcurse/core/include/listselectable.rb
@@ -206,7 +206,7 @@ module RubyCurses
     def list_bindings
       # what about users wanting 32 and ENTER to also go to next row automatically
       # should make that optional, TODO
-      bind_key(32, 'toggle selection') { toggle_row_selection }
+      bind_key($row_selector || 32, 'toggle selection') { toggle_row_selection }
       bind_key(0, 'range select') { add_to_selection }
       bind_key(?+, :ask_select) # --> calls select_values
       bind_key(?-, :ask_unselect) # please implement FIXME TODO
