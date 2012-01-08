293821530bfa8ff1534a3ae7b4917c0e118c94f7
Rahul Kumar <rahulbenegal@gmail.com>
2012-01-08 15:33:05 +0530

    exit on 0, just in case some bug in program

--- examples/testkeypress.rb
+++ examples/testkeypress.rb
@@ -49,7 +49,7 @@ if $0 == __FILE__
       @form.repaint
       @window.wrefresh
       Ncurses::Panel.update_panels
-      while((ch = @window.getchar()) != 999 )
+      while((ch = @window.getchar()) != 0 )
         str = keycode_tos ch
         $log.debug  "#{ch} got (#{str})"
         texta << "#{ch} got (#{str})"
