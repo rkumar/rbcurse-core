0e6bf5542fab3c12b70ada20c0642c2f70603130
Michal Suchanek <hramrach@gmail.com>
2013-03-20 13:28:24 +0100

    #<NoMethodError: undefined method `getbyte' for "o":String>

--- lib/rbcurse/core/widgets/rwidget.rb
+++ lib/rbcurse/core/widgets/rwidget.rb
@@ -30,6 +30,12 @@ require 'rbcurse/core/include/rinputdataevent' # for FIELD 2010-09-11 12:31
 require 'rbcurse/core/include/io'
 require 'rbcurse/core/system/keydefs'
 
+class String
+    if RUBY_VERSION < "1.9" then
+        alias_method  :getbyte, :[]
+    end
+end
+
 BOLD = FFI::NCurses::A_BOLD
 REVERSE = FFI::NCurses::A_REVERSE
 UNDERLINE = FFI::NCurses::A_UNDERLINE
