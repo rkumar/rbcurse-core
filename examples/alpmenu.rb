ad72debcc9eb1f6df063d5e6c3118f2140bccf5e
Michal Suchanek <hramrach@gmail.com>
2013-03-20 13:34:56 +0100

    alpmenu.rb:11: warning: multiple values for a block parameter (2 for 1) from ../lib/rbcurse/core/widgets/rwidget.rb:646

--- examples/alpmenu.rb
+++ examples/alpmenu.rb
@@ -8,35 +8,35 @@ require 'rbcurse/core/util/app'
     stack :margin_top => 10, :margin_left => 15 do
       #w = "Messages".length + 1
       w = 60
-      menulink :text => "&View Dirs", :width => w, :description => "View Dirs in tree"  do |s|
+      menulink :text => "&View Dirs", :width => w, :description => "View Dirs in tree"  do |s, *stuff|
         message "Pressed #{s.text} "
         load './dirtree.rb'
         #require './viewtodo'; todo = ViewTodo::TodoApp.new; todo.run
       end
       blank
-      menulink :text => "&Tabular", :width => w, :description => "Tabula Rasa"  do |s|
+      menulink :text => "&Tabular", :width => w, :description => "Tabula Rasa"  do |s, *stuff|
         message "Pressed #{s.text} "
         load './tabular.rb'
         #require './testtodo'; todo = TestTodo::TodoApp.new; todo.run
       end
       blank
-      menulink :text => "&Messages", :width => w, :description => "View Tasks"  do |s|
+      menulink :text => "&Messages", :width => w, :description => "View Tasks"  do |s, *stuff|
         message "Pressed #{s.text} "
         load './tasks.rb'
       end
       blank
-      menulink :text => "&Database", :width => w, :description => "Database Demo"  do |s|
+      menulink :text => "&Database", :width => w, :description => "Database Demo"  do |s, *stuff|
         message "Pressed #{s.getvalue} "
         load './dbdemo.rb'
       end
       blank
       # somehow ? in mnemonic won't work
-      menulink :text => "&Setup", :width => w, :description => "Configure Alpine options"  do |s|
+      menulink :text => "&Setup", :width => w, :description => "Configure Alpine options"  do |s, *stuff|
         #message "Pressed #{s.text} "
         alert "Not done!"
       end
       blank
-      menulink :text => "&Quit", :width => w, :description => "Quit this application"  do |s|
+      menulink :text => "&Quit", :width => w, :description => "Quit this application"  do |s, *stuff|
         quit
       end
       @form.bind(:ENTER) do |w|
