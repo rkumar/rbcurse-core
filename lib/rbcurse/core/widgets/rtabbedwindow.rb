950bfab79d7b5be5d64ff55f00f633d625f67eb7
Rahul Kumar <rahulbenegal@gmail.com>
2013-04-01 14:04:02 +0530

    added module RubyCurses

--- lib/rbcurse/core/widgets/rtabbedwindow.rb
+++ lib/rbcurse/core/widgets/rtabbedwindow.rb
@@ -9,7 +9,7 @@
   * Author: rkumar (http://github.com/rkumar/rbcurse/)
   * Date: 22.10.11 - 20:35
   * License: Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
-  * Last update:  Sat Oct 22 20:35:46 IST 2011
+  * Last update:  2013-04-01 13:42
 
   == CHANGES
   == TODO 
@@ -19,50 +19,52 @@ require 'rbcurse/core/widgets/rtabbedpane'
 require 'rbcurse/core/widgets/rcontainer'
 
 include RubyCurses
-class TabbedWindow
-  attr_reader :tabbed_pane
-  # The given block is passed to the TabbedPane
-  # The given dimensions are used to create the window.
-  # The TabbedPane is placed at 0,0 and fills the window.
-  def initialize config={}, &block
-    
-    h = config.fetch(:height, 0)
-    w = config.fetch(:width, 0)
-    t = config.fetch(:row, 0)
-    l = config.fetch(:col, 0)
-    @window = VER::Window.new :height => h, :width => w, :top => t, :left => l
-    @form = Form.new @window
-    config[:row] = config[:col] = 0
-    @tabbed_pane = TabbedPane.new @form, config , &block
-  end
-  # returns button index
-  # Call this after instantiating the window
-  def run
-    @form.repaint
-    @window.wrefresh
-    return handle_keys
-  end
-  # returns button index
-  private
-  def handle_keys
-    buttonindex = catch(:close) do 
-      while((ch = @window.getchar()) != FFI::NCurses::KEY_F10 )
-        break if ch == ?\C-q.getbyte(0) 
-        begin
-          @form.handle_key(ch)
-          @window.wrefresh
-        rescue => err
-          $log.debug( err) if err
-          $log.debug(err.backtrace.join("\n")) if err
-          textdialog ["Error in TabbedWindow: #{err} ", *err.backtrace], :title => "Exception"
-          $error_message.value = ""
-        ensure
-        end
+module RubyCurses
+  class TabbedWindow
+    attr_reader :tabbed_pane
+    # The given block is passed to the TabbedPane
+    # The given dimensions are used to create the window.
+    # The TabbedPane is placed at 0,0 and fills the window.
+    def initialize config={}, &block
 
-      end # while loop
-    end # close
-    $log.debug "XXX: CALLER GOT #{buttonindex} "
-    @window.destroy
-    return buttonindex 
+      h = config.fetch(:height, 0)
+      w = config.fetch(:width, 0)
+      t = config.fetch(:row, 0)
+      l = config.fetch(:col, 0)
+      @window = VER::Window.new :height => h, :width => w, :top => t, :left => l
+      @form = Form.new @window
+      config[:row] = config[:col] = 0
+      @tabbed_pane = TabbedPane.new @form, config , &block
+    end
+    # returns button index
+    # Call this after instantiating the window
+    def run
+      @form.repaint
+      @window.wrefresh
+      return handle_keys
+    end
+    # returns button index
+    private
+    def handle_keys
+      buttonindex = catch(:close) do 
+        while((ch = @window.getchar()) != FFI::NCurses::KEY_F10 )
+          break if ch == ?\C-q.getbyte(0) 
+          begin
+            @form.handle_key(ch)
+            @window.wrefresh
+          rescue => err
+            $log.debug( err) if err
+            $log.debug(err.backtrace.join("\n")) if err
+            textdialog ["Error in TabbedWindow: #{err} ", *err.backtrace], :title => "Exception"
+            $error_message.value = ""
+          ensure
+          end
+
+        end # while loop
+      end # close
+      $log.debug "XXX: CALLER GOT #{buttonindex} "
+      @window.destroy
+      return buttonindex 
+    end
   end
 end
